6a505bed6d6d70f96c75ac8ff8a65882240719a9
“Bruskiguy” <caleb.a.bruski@gmail.com>
2021-07-15 16:13:52 +0300

    logic working (with bugs)

--- scripts.js
+++ scripts.js
@@ -1,23 +1,40 @@
 
 
-const rps = ["rock", "paper", "scissors"];
+const rps = ["ROCK", "PAPER", "SCISSORS"];
 
-let computerPlay =  Math.floor(Math.random() * rps.length);
+let playerSelection = window.prompt("ROCK, PAPER, or SCISSORS?")
 
-console.log (rps[computerPlay]);
+let comSel =  Math.floor(Math.random() * rps.length);
+
+let computerPlay = (rps[comSel]);
+
+console.log(rps[computerPlay]);
 
 
+console.log(playerSelection, computerPlay);
+/* Need to change both to strings? */
 
+/* Game Logic*/
 
-/*
-let gamerChoice = input choice of array [rock,paper,scissors];
-
-if user input = scissors 
-
-function play (computerChoice, gamerChoice)
-if (gamerChoice = scissors) {
-    computer selected paper = win
-    computer selected rock = loss
-    computer selected scissors = draw
-}
-*/
+if (playerSelection === "ROCK") {
+    if (computerPlay === "ROCK")
+        alert("Tie: Both selected ROCK");
+    else if (computerPlay === "PAPER")
+        alert("You Lose! PAPER beats ROCK");
+    else (computerPlay === "SCISSORS")
+        alert("You Won! ROCK beats SCISSORS");
+} 
+else if (playerSelection === "PAPER") {
+    if (computerPlay === "PAPER")
+        alert("TIE: Both selected PAPER");
+    else if (computerPlay === "ROCK")
+        alert("You Won! PAPER beats ROCK");
+    else (computerPlay === "SCISSORS")
+        alert("You Lose! SCISSORS beats PAPER");
+} else if (playerSelection === "SCISSORS") {
+    if (computerPlay === "SCISSORS")
+        alert("TIE: Both selected SCISSORS");
+    else if (computerPlay === "PAPER")
+        alert("You Won! SCISSORS beats PAPER");
+    else if (computerPlay === "ROCK")
+        alert ("You Lose! ROCK beats SCISSORS");}
