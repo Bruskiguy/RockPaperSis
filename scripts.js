3fe3e8b2faaca97c70571c76c3fea53bf58910a0
“Bruskiguy” <caleb.a.bruski@gmail.com>
2021-07-15 14:40:35 +0300

    computerPlay selects random array object

--- scripts.js
+++ scripts.js
@@ -1,1 +1,23 @@
-console.log ("hello my friend.")
+
+
+const rps = ["rock", "paper", "scissors"];
+
+let computerPlay =  Math.floor(Math.random() * rps.length);
+
+console.log (rps[computerPlay]);
+
+
+
+
+/*
+let gamerChoice = input choice of array [rock,paper,scissors];
+
+if user input = scissors 
+
+function play (computerChoice, gamerChoice)
+if (gamerChoice = scissors) {
+    computer selected paper = win
+    computer selected rock = loss
+    computer selected scissors = draw
+}
+*/
